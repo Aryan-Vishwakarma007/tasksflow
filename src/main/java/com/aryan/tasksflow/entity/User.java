0086368514f81a1d6f92c7cb3fc2cbfbf1ed7a2d
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-06-30 17:39:17 +0530

    feilds of User  entity added

--- src/main/java/com/aryan/tasksflow/entity/User.java
+++ src/main/java/com/aryan/tasksflow/entity/User.java
@@ -1,4 +1,16 @@
 package com.aryan.tasksflow.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
 public class User {
+    @Id
+    private String id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
 }
