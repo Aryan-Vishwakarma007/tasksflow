954df9c71f8e1b1dc1eb546856da1ed648760449
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-06-30 17:36:50 +0530

    feilds of task getter setter entity added

--- src/main/java/com/aryan/tasksflow/entity/Task.java
+++ src/main/java/com/aryan/tasksflow/entity/Task.java
@@ -21,4 +21,60 @@ public class Task {
     private LocalDate deadline;
 
     private String userId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
