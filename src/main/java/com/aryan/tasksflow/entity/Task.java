c1145d9763e1c083264397822e96ecf072893b3a
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-01 17:50:18 +0530

    added the use of lombok

--- src/main/java/com/aryan/tasksflow/entity/Task.java
+++ src/main/java/com/aryan/tasksflow/entity/Task.java
@@ -1,11 +1,13 @@
 package com.aryan.tasksflow.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
 @Document(collection = "tasks")
+@Data
 public class Task {
     @Id
     private String id;
@@ -22,59 +24,5 @@ public class Task {
 
     private String userId;
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getPriority() {
-        return priority;
-    }
-
-    public void setPriority(String priority) {
-        this.priority = priority;
-    }
-
-    public LocalDate getDeadline() {
-        return deadline;
-    }
-
-    public void setDeadline(LocalDate deadline) {
-        this.deadline = deadline;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
 }
