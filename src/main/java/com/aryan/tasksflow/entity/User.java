f780da728076d73adf8ace8a6db21ae5657b7216
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-01 17:50:26 +0530

    added the use of lombok

--- src/main/java/com/aryan/tasksflow/entity/User.java
+++ src/main/java/com/aryan/tasksflow/entity/User.java
@@ -1,9 +1,11 @@
 package com.aryan.tasksflow.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
+@Data
 public class User {
     @Id
     private String id;
@@ -14,35 +16,4 @@ public class User {
 
     private String password;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
