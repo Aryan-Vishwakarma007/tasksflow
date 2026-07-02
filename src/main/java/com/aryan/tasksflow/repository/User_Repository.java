33a86726dda60954f6bfa0c93e7903bb0b3550a2
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-02 17:50:47 +0530

    new

--- src/main/java/com/aryan/tasksflow/repository/User_Repository.java
+++ src/main/java/com/aryan/tasksflow/repository/User_Repository.java
@@ -1,4 +1,6 @@
 package com.aryan.tasksflow.repository;
 
-public class User_Repository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface User_Repository extends MongoRepository {
 }
