929bc9c1792df9f2c8ac86f4ca005de8b8e43ccd
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-02 17:55:15 +0530

    new

--- src/main/java/com/aryan/tasksflow/controller/User_Controller.java
+++ src/main/java/com/aryan/tasksflow/controller/User_Controller.java
@@ -1,10 +1,22 @@
 package com.aryan.tasksflow.controller;
 
+import com.aryan.tasksflow.entity.User;
+import com.aryan.tasksflow.services.User_Services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class User_Controller {
+    @Autowired
+    User_Services userServices;
 
+    @GetMapping
+    public List<User> getall(){
+        userServices.getalls();
+    }
 }
