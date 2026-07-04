b027857940a013a2a5ae5edcfa8a3365de180515
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-04 18:04:47 +0530

    get tasks

--- src/main/java/com/aryan/tasksflow/services/Task_Services.java
+++ src/main/java/com/aryan/tasksflow/services/Task_Services.java
@@ -1,4 +1,17 @@
 package com.aryan.tasksflow.services;
+import com.aryan.tasksflow.entity.Task;
+import com.aryan.tasksflow.repository.Task_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class Task_Services {
+    @Autowired
+    private Task_Repository taskRepository;
+
+    public List<Task> getTasks(){
+        return taskRepository.findAll();
+    }
 }
