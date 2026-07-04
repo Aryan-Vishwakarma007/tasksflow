3f5dbdfc1466f2b6d2afb4ffd7ddf2b237daf7ac
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-04 18:04:38 +0530

    get tasks

--- src/main/java/com/aryan/tasksflow/controller/Task_Controller.java
+++ src/main/java/com/aryan/tasksflow/controller/Task_Controller.java
@@ -1,4 +1,23 @@
 package com.aryan.tasksflow.controller;
 
+import com.aryan.tasksflow.entity.Task;
+import com.aryan.tasksflow.services.Task_Services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class Task_Controller {
+    @Autowired
+    private Task_Services taskServices;
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getTasks(){
+        return new ResponseEntity<>(taskServices.getTasks(), HttpStatus.OK);
+    }
+
 }
