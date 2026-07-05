6b7f77dbfa2ec8799329d70f53892b940fea696e
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-05 18:41:37 +0530

    post call

--- src/main/java/com/aryan/tasksflow/repository/Task_Repository.java
+++ src/main/java/com/aryan/tasksflow/repository/Task_Repository.java
@@ -5,4 +5,5 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface Task_Repository extends MongoRepository<Task, String> {
+//    public Task findById(String myId);
 }
