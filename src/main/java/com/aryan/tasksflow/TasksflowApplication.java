7f7d37d962f3c98b817e16b90684340b57fb54c4
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-06 19:56:47 +0530

    Adding @Transational annotation

--- src/main/java/com/aryan/tasksflow/TasksflowApplication.java
+++ src/main/java/com/aryan/tasksflow/TasksflowApplication.java
@@ -2,12 +2,21 @@ package com.aryan.tasksflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class TasksflowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksflowApplication.class, args);
 	}
-
+		@Bean
+		public PlatformTransactionManager transaction(MongoDatabaseFactory db){
+		return new MongoTransactionManager(db);
+		}
 }
