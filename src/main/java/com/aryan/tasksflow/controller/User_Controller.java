9a7772d0711f4ac40c8d236529ba6cdbdfb571c8
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-06 19:56:40 +0530

    Adding @Transational annotation

--- src/main/java/com/aryan/tasksflow/controller/User_Controller.java
+++ src/main/java/com/aryan/tasksflow/controller/User_Controller.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -45,6 +46,7 @@ public class User_Controller {
         }
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
+    @Transactional
     @DeleteMapping("/{myId}")
     public ResponseEntity<?> Delete_By_Id(@PathVariable String myId){
         userServices.deleteUser(myId);
