489e8756b46d818c4baaf9d246851608021de4b5
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-10 17:26:37 +0530

    added spring security

--- src/main/java/com/aryan/tasksflow/config/SpringSecurity.java
+++ src/main/java/com/aryan/tasksflow/config/SpringSecurity.java
@@ -1,4 +1,9 @@
 package com.aryan.tasksflow.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SpringSecurity {
 }
