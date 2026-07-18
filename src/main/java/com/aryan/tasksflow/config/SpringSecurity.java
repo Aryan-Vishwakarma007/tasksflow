bde74f53cb62d0f9d24cd8986d009c55753c5a67
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-18 17:03:01 +0530

    not dead :)

--- src/main/java/com/aryan/tasksflow/config/SpringSecurity.java
+++ src/main/java/com/aryan/tasksflow/config/SpringSecurity.java
@@ -2,7 +2,9 @@ package com.aryan.tasksflow.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,5 +26,11 @@ public class SpringSecurity {
 
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
