05e9b6fda1a0b71893c43d99d89ab74993cf4a64
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-18 17:19:54 +0530

    Spring Security added

--- src/main/java/com/aryan/tasksflow/controller/Health_Check.java
+++ src/main/java/com/aryan/tasksflow/controller/Health_Check.java
@@ -1,4 +1,10 @@
 package com.aryan.tasksflow.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/check")
 public class Health_Check {
+
 }
