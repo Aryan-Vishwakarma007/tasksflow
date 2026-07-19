ab4e04ba2c15d07ddaa33cd7d0c6f66d11afa7ab
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-19 17:57:07 +0530

    Spring Security adding

--- src/main/java/com/aryan/tasksflow/repository/Task_Repository.java
+++ src/main/java/com/aryan/tasksflow/repository/Task_Repository.java
@@ -4,7 +4,11 @@ import com.aryan.tasksflow.entity.Task;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface Task_Repository extends MongoRepository<Task, String> {
 //    public Task findById(String myId);
         void deleteByuserId(String userId);
+
+    List<Task> findByuserId(String myId);
 }
