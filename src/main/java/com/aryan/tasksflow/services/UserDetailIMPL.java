01d22a66330b2ed9c09c62da3e50335fdaaf0ed8
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-22 19:34:42 +0530

    Spring Security adding

---
+++ src/main/java/com/aryan/tasksflow/services/UserDetailIMPL.java
@@ -0,0 +1,35 @@
+package com.aryan.tasksflow.services;
+
+import com.aryan.tasksflow.entity.User;
+import com.aryan.tasksflow.repository.User_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+@Component
+@Service
+public class UserDetailIMPL implements UserDetailsService {
+    @Autowired
+    User_Repository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User byUser = userRepository.findByusername(username);
+        if(byUser != null){
+            UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
+
+                    .username(byUser.getUsername())
+                    .password(byUser.getPassword())
+                    .roles(byUser.getRoles().toArray(new String[0]))
+                    .build();
+
+            return userDetails;
+        }
+
+
+        throw new UsernameNotFoundException("User Not Found!") ;
+    }
+}
