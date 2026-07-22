6660976bd390e2f62d6050f3a59af691b401356e
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-22 19:34:34 +0530

    Spring Security adding

--- src/main/java/com/aryan/tasksflow/services/User_Services.java
+++ src/main/java/com/aryan/tasksflow/services/User_Services.java
@@ -2,6 +2,8 @@ package com.aryan.tasksflow.services;
 import com.aryan.tasksflow.entity.User;
 import com.aryan.tasksflow.repository.User_Repository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,12 +16,21 @@ public class User_Services {
     @Autowired
     Task_Services taskServices;
 
+    private static final PasswordEncoder encoderr = new BCryptPasswordEncoder();
+
     public List<User> getalls(){
         return userRepository.findAll();
     }
 
     public void setnew(User myUser){
         userRepository.save(myUser);
+    }
+
+    public void  setnewUser(User Myuser){
+
+        Myuser.setPassword(encoderr.encode(Myuser.getPassword()));
+        userRepository.save(Myuser);
+
     }
 
     public User findByusername(String username){
