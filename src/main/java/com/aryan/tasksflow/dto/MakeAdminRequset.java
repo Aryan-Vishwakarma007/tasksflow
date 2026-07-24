22ae2946ee094af5feaca391f0a45ed6e84ef263
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-24 16:21:06 +0530

    added Data Transfer Object

---
+++ src/main/java/com/aryan/tasksflow/dto/MakeAdminRequset.java
@@ -0,0 +1,10 @@
+package com.aryan.tasksflow.dto;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class MakeAdminRequset {
+    private String username;
+}
