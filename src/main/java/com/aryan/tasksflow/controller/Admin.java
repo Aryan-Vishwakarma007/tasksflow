6c3600e55dd5d79e98e0b091119bbe0e1dfd38df
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-24 15:42:30 +0530

    adding role based auth

--- src/main/java/com/aryan/tasksflow/controller/Admin.java
+++ src/main/java/com/aryan/tasksflow/controller/Admin.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -24,5 +26,12 @@ public class Admin {
             return  new ResponseEntity<>(users, HttpStatus.FOUND);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/make-user-admin")
+    public  ResponseEntity<?> makeAdmin(@RequestBody String name){
+        User user = userServices.findByusername(name);
+        user.setRoles(Arrays.asList("ADMIN"));
+        userServices.setnew(user);
     }
 }
