3a5b5223b79eb358e848923e5fab20fb22c9cce6
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-07-29 18:17:19 +0530

    fix: duplicate names

--- src/main/java/com/aryan/tasksflow/entity/User.java
+++ src/main/java/com/aryan/tasksflow/entity/User.java
@@ -2,6 +2,7 @@ package com.aryan.tasksflow.entity;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.List;
 public class User {
     @Id
     private String id;
-
+    @Indexed(unique = true)
     private String username;
 
     private String email;
