28f3c5c8ab41ea632d7be6756df72214b6d4a19c
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-08-01 16:04:38 +0530

    fix: admin based tasks assigning

--- src/main/java/com/aryan/tasksflow/repository/Task_Repository.java
+++ src/main/java/com/aryan/tasksflow/repository/Task_Repository.java
@@ -1,5 +1,6 @@
 package com.aryan.tasksflow.repository;
 
+import com.aryan.tasksflow.dto.AssignGroupTaskRequest;
 import com.aryan.tasksflow.entity.Priority;
 import com.aryan.tasksflow.entity.Task;
 import org.bson.types.ObjectId;
@@ -11,8 +12,11 @@ public interface Task_Repository extends MongoRepository<Task, String> {
 //    public Task findById(String myId);
         void deleteByuserId(String userId);
 
+//        List<Task> saveGroupTask(AssignGroupTaskRequest tasks);
     List<Task> findByuserId(String myId);
 
     List<Task> findByUserIdAndPriority(String userId, Priority priority);
 
+    List<Task> saveTasks(AssignGroupTaskRequest myTask);
 }
+
