723959f925ee533dc2c4a605597f4c492373c4ee
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-08-08 20:58:46 +0530

    adding "sent for review" system for task

---
+++ src/main/java/com/aryan/tasksflow/services/EmailService.java
@@ -0,0 +1,26 @@
+package com.aryan.tasksflow.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.service.registry.ImportHttpServices;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EmailService {
+
+    @Autowired
+    JavaMailSender javaMailSender;
+    public void sendEmail(String to, String subject, String body) {
+        try {
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(to);
+            mail.setSubject(subject);
+            mail.setText(body);
+            javaMailSender.send(mail);
+        } catch (Exception e) {
+            System.out.println("Exception while sendEmail ");
+        }
+    }
+}
