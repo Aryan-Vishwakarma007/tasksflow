35788cc8b9869bc41e650d7898fd8e6ad90f4055
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-08-18 22:12:44 +0530

    ADDING JWT WITH SPRING 6

--- src/main/java/com/aryan/tasksflow/controller/Public.java
+++ src/main/java/com/aryan/tasksflow/controller/Public.java
@@ -1,11 +1,15 @@
 package com.aryan.tasksflow.controller;
 
 import com.aryan.tasksflow.entity.User;
+import com.aryan.tasksflow.services.UserDetailIMPL;
 import com.aryan.tasksflow.services.User_Services;
+import com.aryan.tasksflow.utills.JWTUtills;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +22,11 @@ public class Public {
     User_Services userServices;
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    JWTUtills jwtUtills;
+    @Autowired
+    UserDetailIMPL userDetailIMPL;
+
 
     @PostMapping("/sign-up")
     public ResponseEntity<?> signup(@RequestBody User myUser){
@@ -32,6 +41,15 @@ public class Public {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody User myUser){
-
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(myUser.getUsername(), myUser.getPassword()));
+            // above line will while logging in kya user name and password sahi h ki nhii
+            UserDetails userDetails = userDetailIMPL.loadUserByUsername(myUser.getUsername());
+            // user ki detailss dedo bhai
+            String jwt = jwtUtills.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
