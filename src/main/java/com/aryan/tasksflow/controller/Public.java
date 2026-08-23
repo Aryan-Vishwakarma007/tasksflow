8d431a9714b6f18740ea9328efcd31035abddac6
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-08-23 20:15:07 +0530

    ADDING JWT WITH SPRING 6

--- src/main/java/com/aryan/tasksflow/controller/Public.java
+++ src/main/java/com/aryan/tasksflow/controller/Public.java
@@ -4,6 +4,7 @@ import com.aryan.tasksflow.entity.User;
 import com.aryan.tasksflow.services.UserDetailIMPL;
 import com.aryan.tasksflow.services.User_Services;
 import com.aryan.tasksflow.utills.JWTUtills;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/public")
+@Slf4j
 public class Public {
     @Autowired
     User_Services userServices;
@@ -42,6 +44,7 @@ public class Public {
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody User myUser){
         try{
+
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(myUser.getUsername(), myUser.getPassword()));
             // above line will while logging in kya user name and password sahi h ki nhii
             UserDetails userDetails = userDetailIMPL.loadUserByUsername(myUser.getUsername());
@@ -49,7 +52,8 @@ public class Public {
             String jwt = jwtUtills.generateToken(userDetails.getUsername());
             return new ResponseEntity<>(jwt, HttpStatus.OK);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            log.error("Error while creating token", e);
+            return new ResponseEntity<>("Incorrect Name or Password", HttpStatus.BAD_REQUEST);
         }
     }
 }
